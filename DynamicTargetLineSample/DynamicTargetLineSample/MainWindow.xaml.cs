32671e31075f762d61683015edfca6264b6f3ecd
tineshkumar.sf4652 <tineshkumar.sf4652@gmail.com>
2024-12-15 22:49:02 +0530

    attach the sample

---
+++ DynamicTargetLineSample/DynamicTargetLineSample/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DynamicTargetLineSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _previousValidText = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Y_Axis == null) return;
+            var maxValue = Y_Axis.Maximum;
+
+            if (sender is TextBox textBox)
+            {
+                textBox.TextChanged -= TextBox_TextChanged;
+
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    viewModel.Y1 = double.MinValue;
+                    textBox.Text = string.Empty;
+                }
+                else
+                {
+                    if (int.TryParse(textBox.Text, out int newValue))
+                    {
+                        if (newValue > maxValue)
+                            newValue = (int)maxValue;
+                        else if (newValue < 0)
+                            newValue = 0;
+
+                        viewModel.Y1 = newValue;
+
+                        textBox.Text = newValue.ToString();
+                        textBox.CaretIndex = textBox.Text.Length;
+                    }
+                    else
+                    {
+                        textBox.Text = ((int)viewModel.Y1).ToString();
+                        textBox.CaretIndex = textBox.Text.Length;
+                    }
+                }
+
+                textBox.TextChanged += TextBox_TextChanged;
+            }
+        }
+    }
+}
---
+++ DynamicTargetLineSample/DynamicTargetLineSample/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DynamicTargetLineSample
+{
+    internal class ViewModel:INotifyPropertyChanged
+    {
+        private double y1;
+        public double Y1
+        {
+            get => y1;
+            set
+            {
+                if(y1 != value)
+                {
+                    y1 = value;
+                    OnPropertyChanged(nameof(Y1));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ViewModel()
+        {
+            Y1 = 12000;
+            Data = new ObservableCollection<Model>()
+            {
+                new Model() { Months = "January", Revenue = 10000 },
+                new Model() { Months = "February", Revenue = 13500 },
+                new Model() { Months = "March", Revenue = 16000 },
+                new Model() { Months = "April", Revenue = 14000 },
+                new Model() { Months = "May", Revenue = 12500 },
+                new Model() { Months = "June", Revenue = 18000 },
+                new Model() { Months = "July", Revenue = 11700 }
+            };
+        }
+    }
+}
